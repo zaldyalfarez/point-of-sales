a5556350e6f31edec3a8354e776ea4a5c47193b8
Rizaldy Alfarez <zaldyalfarez@gmail.com>
2026-03-31 15:52:03 +0700

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/point-of-sales",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
